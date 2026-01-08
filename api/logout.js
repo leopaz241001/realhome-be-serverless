06ce2f54c818758aec0e1f4b492f84f7cc2d884a
딩 룡_Dinh Long <longpd@hivelab.com>
2026-01-08 09:39:29 +0700

    add check method logout

--- api/logout.js
+++ api/logout.js
@@ -3,9 +3,14 @@ import { applyCors } from "../lib/cors";
 export default function handler(req, res) {
   if(applyCors(req, res)) return;
   
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  
   res.setHeader(
     'Set-Cookie',
     'access_token=; Path=/; Max-Age=0; HttpOnly; SameSite=None; Secure'
   );
-  res.json({ message: 'Logged out' });
+
+  res.json({ message: 'Đã đăng xuất.' });
 }
