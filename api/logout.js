bc79421e1ca09b7ebe5f86819015e29d356b365f
딩 룡_Dinh Long <longpd@hivelab.com>
2026-01-08 09:18:58 +0700

    add cors logout

--- api/logout.js
+++ api/logout.js
@@ -1,4 +1,8 @@
+import { applyCors } from "../lib/cors";
+
 export default function handler(req, res) {
+  if(applyCors(req, res)) return;
+  
   res.setHeader(
     'Set-Cookie',
     'access_token=; Path=/; Max-Age=0; HttpOnly; SameSite=None; Secure'
